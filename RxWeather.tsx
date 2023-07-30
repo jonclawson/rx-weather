b0216e0cf4b0675cb65c1b27088f6f7c4698732a
jonclawson <30538332+jonclawson@users.noreply.github.com>
2023-07-29 18:35:57 -0700

    Styles

--- RxWeather.tsx
+++ RxWeather.tsx
@@ -11,14 +11,16 @@ interface RxWeatherProps {
   weather: RxWeatherResponse;
   fetchWeather: typeof RxWeatherLoad.strictGet;
 }
-mapboxgl.accessToken = 'xxxx';
-const Map = ReactMapboxGl({
-  accessToken: mapboxgl.accessToken,
-});
+// mapboxgl.accessToken = 'xxxx';
+// const Map = ReactMapboxGl({
+//   accessToken: mapboxgl.accessToken,
+// });
 
 export class RxWeather extends Component<RxWeatherProps, RxWeatherState> {
   name: string;
   input$ = new Subject<string>();
+
+  icon = (src) => <img id="wicon" src="{src}" alt="Weather icon" />;
 
   constructor(props) {
     super(props);
@@ -40,41 +42,56 @@ export class RxWeather extends Component<RxWeatherProps, RxWeatherState> {
   }
 
   render() {
+    console.log(JSON.stringify(this.props.weather, null, ' '));
     return (
       <div className="row">
         <div className="col">
-          <h1> {this.name}</h1>
-
-          <input
-            className="form-control"
-            type="text"
-            placeholder="Enter a City"
-            defaultValue={this.props.weather?.name}
-            onChange={this.handleQueryInput}
-          />
-
+          <div>
+            <h1> {this.name}</h1>
+            <input
+              className="form-control"
+              type="text"
+              placeholder="Enter a City"
+              defaultValue={this.props.weather?.name}
+              onChange={this.handleQueryInput}
+            />
+          </div>
           {this.props.weather ? (
-            <div>
-              <p>
-                Currently:{' '}
-                {this.props.weather?.weather?.map((w) => w.description)}
-              </p>
-              <p>
-                <span className="badge bg-primary">
-                  {' '}
-                  {this.props.weather?.main?.temp}°F
-                </span>
-                , feels like: {this.props.weather?.main?.feels_like}°F, low:{' '}
-                {this.props.weather?.main.temp_min}, high:{' '}
-                {this.props.weather?.main.temp_max}, Humidity:{' '}
-                {this.props.weather?.main.humidity}, Presure:{' '}
-                {this.props.weather?.main.pressure}, Wind:{' '}
-                {this.props.weather?.wind.speed} mph, Sunrise:{' '}
-                {this.readTimeStamp(this.props.weather?.sys.sunrise)} , Sunset:{' '}
-                {this.readTimeStamp(this.props.weather?.sys.sunset)} ,
-              </p>
+            <div className="mt-3">
+              <div className="row">
+                <div className="col-6">
+                  {this.props.weather?.weather?.map((w) => (
+                    <h1>
+                      {w.description}{' '}
+                      <img
+                        className=""
+                        src={`//openweathermap.org/img/wn/${w.icon}@2x.png`}
+                      />
+                      <span className="badge bg-warning">
+                        {this.props.weather?.main?.temp.toFixed(0)}°F
+                      </span>
+                    </h1>
+                  ))}
+  
+                </div>
+                <div className="col-6">
+                  <div>
+                    feels like: {this.props.weather?.main?.feels_like}°F{' '}
+                  </div>
+                  <div>low: {this.props.weather?.main.temp_min} </div>
+                  <div>high: {this.props.weather?.main.temp_max} </div>
+                  <div>Humidity: {this.props.weather?.main.humidity} </div>
+                  <div>Presure: {this.props.weather?.main.pressure} </div>
+                  <div>Wind: {this.props.weather?.wind.speed} mph </div>
+                </div>
+              </div>
 
-              <Map
+              <div>
+                Sunrise: {this.readTimeStamp(this.props.weather?.sys.sunrise)} ,
+                Sunset: {this.readTimeStamp(this.props.weather?.sys.sunset)} ,
+              </div>
+
+              {/* <Map
                 style="mapbox://styles/mapbox/streets-v9" // eslint-disable-line
                 containerStyle={{
                   height: '90vh',
@@ -85,8 +102,7 @@ export class RxWeather extends Component<RxWeatherProps, RxWeatherState> {
                   this.props.weather.coord.lon,
                   this.props.weather.coord.lat,
                 ]}
-              ></Map>
-              {/* <pre>{JSON.stringify(this.props?.weather, 1, ' ')}</pre> */}
+              ></Map> */}
             </div>
           ) : (
             ''
