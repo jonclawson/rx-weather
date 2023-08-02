09f88cbbf14ead5b3f50e99fceba4d6ef764f4c3
jonclawson <30538332+jonclawson@users.noreply.github.com>
2023-08-02 01:27:58 -0700

    mapbox

--- src/mapbox.js
+++ src/mapbox.js
@@ -1,7 +1,6 @@
 import * as mapbox_gl from 'mapbox-gl';
+const token = 'pk.eyJ1Ijoiam9uY2xhd3NvbiIsImEiOiJjbGtycWsxZXUxZzUwM2Zwcm9pb3loaDdwIn0.ov_v9NV9rGjLnonE0uGXfA';
+mapbox_gl.accessToken =
+  token;
 
-export const key = 'pk.eyJ1Ijoiam9uY2xhd3NvbiIsImEiOiJjbGtycWsxZXUxZzUwM2Zwcm9pb3loaDdwIn0.ov_v9NV9rGjLnonE0uGXfA';
-
-mapbox_gl.accessToken = key;
-
-export const mapboxGl = mapbox_gl;
+export default {mapbox_gl, key: token };
