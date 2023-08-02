0fca4eaea87528ee13ee5761d0bd77ccc17b2b53
jonclawson <30538332+jonclawson@users.noreply.github.com>
2023-08-02 01:54:46 -0700

    remove mapbox

--- src/mapbox.js
+++ src/mapbox.js
@@ -1,6 +1,6 @@
-import * as mapbox_gl from 'mapbox-gl';
+// import * as mapbox_gl from 'mapbox-gl';
 const token = 'pk.eyJ1Ijoiam9uY2xhd3NvbiIsImEiOiJjbGtycWsxZXUxZzUwM2Zwcm9pb3loaDdwIn0.ov_v9NV9rGjLnonE0uGXfA';
-mapbox_gl.accessToken =
-  token;
+// mapbox_gl.accessToken =
+//   token;
 
 export default {key: token };
